706ba63046090ace2728275d15f25431db1a5e75
HumanDev <motocycle9719@hotmail.com>
2025-03-28 00:34:15 -0600

    feat: update database migration neon

---
+++ Controllers/PostController.cs
@@ -0,0 +1,73 @@
+using BlogPost.Api.DTOs;
+using BlogPost.Api.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogPost.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] // Ruta base: api/posts
+public class PostController : ControllerBase
+{
+    private readonly IPostService _postservice;
+
+    // Inyectamos el servicio en el constructor
+    public PostController(IPostService postservice)
+    {
+        _postservice = postservice;
+    }
+
+    // GET: api/posts
+    // Obtiene todos los posts
+    [HttpGet]
+    public async Task<IActionResult> GetPosts()
+    {
+        var posts = await _postservice.GetPostsAsync();
+        return Ok(posts);
+    }
+
+    public async Task<IActionResult> GetPost(Guid id)
+    {
+        var post = await _postservice.GetPostByIdAsync(id);
+        if (post is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(post);
+    }
+
+
+    // POST: api/post
+    [HttpPost]
+    public async Task<IActionResult> CreatePost([FromBody] CreatePostDto dto)
+    {
+        var createdPost = await _postservice.CreatePostAsync(dto);
+        return CreatedAtAction(nameof(GetPost), new { id = createdPost.Id }, createdPost);
+        //HTTP 201
+    }
+
+    // PUT: api/posts/{id}
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdatePost(Guid id, [FromBody] UpdatePostDto dto)
+    {
+        var update = await _postservice.UpdatePostAsync(id, dto);
+        if (!update)
+            return NotFound();
+
+        // HTTP 204 indicando exito sin contenido adicional
+        return NoContent();
+    }
+
+    // DELETE: api/posts/{id}
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeletePost(Guid id)
+    {
+        var deleted = await _postservice.DeletePostAsync(id);
+        if (!deleted)
+            return NotFound();
+
+        return NoContent();
+    }
+
+
+}
---
+++ Migrations/20250328012312_InitialCreate.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlogPost.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Content = table.Column<string>(type: "text", nullable: false),
+                    Author = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdateAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Comments",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<string>(type: "text", nullable: false),
+                    UserName = table.Column<string>(type: "text", nullable: false),
+                    UserEmail = table.Column<string>(type: "text", nullable: false),
+                    Content = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    PostId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Comments_Posts_PostId",
+                        column: x => x.PostId,
+                        principalTable: "Posts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_PostId",
+                table: "Comments",
+                column: "PostId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comments");
+
+            migrationBuilder.DropTable(
+                name: "Posts");
+        }
+    }
+}
